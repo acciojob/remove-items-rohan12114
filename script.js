7eb70f865e1d8d838eba5b869600dbc0d9eecc29
rohan12114 <122142354+rohan12114@users.noreply.github.com>
2023-06-26 14:58:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+function removeColor() {
+  // Get the select element by its id
+  var selectElement = document.getElementById("colorSelect");
+
+  // Get the selected option
+  var selectedOption = selectElement.options[selectElement.selectedIndex];
+
+  // Remove the selected option
+  selectElement.removeChild(selectedOption);
+}
